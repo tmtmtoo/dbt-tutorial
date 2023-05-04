81b2d8b443b995a6c86e199b81cd035418b85fc4
matsu <tmtmt2209@gmail.com>
2023-05-04 13:26:15 +0000

    update tutorial model

--- models/example2/customers.sql
+++ models/example2/customers.sql
@@ -1,17 +1,8 @@
 with customers as (
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from dbt_tmtmtoo.raw_customers
+    select * from {{ ref('stg_customers') }}
 ),
 orders as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from dbt_tmtmtoo.raw_orders
+    select * from {{ ref('stg_orders') }}
 ),
 customer_orders as (
     select
