53e8ff3b050da8cc6922228e399d3c992841757d
matsu <tmtmt2209@gmail.com>
2023-05-06 18:12:20 +0900

    Use jinja (#3)
    * use for loop
    
    * use macro

---
+++ models/example2/order_payment_method_amouts.sql
@@ -0,0 +1,11 @@
+{% set payment_methods = get_payment_methods() %}
+
+select
+    order_id,
+    {% for payment_method in payment_methods %}
+        sum(case when payment_method = '{{ payment_method }}' then amount end) as {{ payment_method }}_amount
+        {% if not loop.last %},{% endif %}
+    {% endfor %}
+from
+    {{ ref('raw_payments') }}
+group by 1
